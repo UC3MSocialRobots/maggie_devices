a14128a0eb2ea6913f55941241e9c7f0870aefbe
Raul Perula-Martinez <raul@raul-laptop.(none)>
2015-03-26 09:56:45 +0100

    removed unnecesary script

--- ir_controller/scripts/rosdep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# External, ROS and system package dependencies
-
-# install headers and lib
-cd ../third_parties/IRTrans_shlib
-sudo sh install.sh
-cd ..
-
-echo "Installed. Now execute config_driver.sh script"
