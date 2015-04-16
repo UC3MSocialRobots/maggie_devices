52bca94a7d54b6e9b65a3d989c820231b4095b16
Marco Faroni <marfa@venezuela.uc3m.es>
2015-04-16 12:11:44 +0200

    corrections

--- maggie_motor_controller/src/motor_controller_data.h
+++ maggie_motor_controller/src/motor_controller_data.h
@@ -45,7 +45,7 @@
 #define TOTAL_ARMS_REDUCTION        (PULSES_PER_REV * ARMS_REDUCTION_FACTOR)
 
 // home position of the motors from the limit switch after the calibration in rads
-#define NECK_HOR_HOME_POS           -1.6
+#define NECK_HOR_HOME_POS           -0.2
 #define NECK_VER_HOME_POS           -0.6
 
 //////////////////////////////////////////////////
